a91284ff608bee0cce4afe527a35dbf6f71e02fb
Tokupensx07 <119869342+tokupens@users.noreply.github.com>
2023-11-29 20:11:10 +0700

    Update hollez.py

--- hollez.py
+++ hollez.py
@@ -325,10 +325,10 @@ def start(nomor,x): # Def Untuk Start Tools
         
 def main():
     os.system("cls") # Clear Terminal agar CMD berwarna
-    autoketik(f"Selamat datang di {merah}MySpamBot")
-    print(f"""{kuning}Author      : {hijau}Ricky Khairul Faza
-{kuning}Github      : {merah}github.com/rickyfazaa
-{kuning}Instagram   : {biru}instagram.com/rickyfazaa""")
+    autoketik(f"Selamat datang bro di {merah}number phone hacked")
+    print(f"""{kuning}Author      : {hijau}holl.ez
+{kuning}Github      : {merah}github.com/tokupens
+{kuning}Instagram   : {biru}instagram.com/holl.ez""")
     # Contoh : 089508226367
     print(nomor := input(f"{hijau}Masukkan Nomor Target: {putih}")) # Walrus Operator untuk inputan Nomor Target
     start(nomor,0) # Memulai Tools
